97172dc34061a779c59fcc206467ce1bc520fb39
asankhaperera <36698455+asankhaperera@users.noreply.github.com>
2018-04-19 23:09:34 +0530

    test

--- APIGatewayExamples/lambda.js
+++ APIGatewayExamples/lambda.js
@@ -2,7 +2,6 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
 	console.log("Event: " + JSON.stringify(event));
-	console.log("Context: " + JSON.stringify(context));
 
 	callback(null,'Successfully executed');
 }
